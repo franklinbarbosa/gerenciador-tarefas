025a3dc794e071785524920b12d8832738283f74
fraklinbarbosa <fbfrank2003@gmail.com>
2019-11-30 16:21:48 -0300

    adicionou index

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -8,6 +8,10 @@ TAREFAS = [{"id": 1, "titulo": "Comprar ovos"},
            {"id": 1, "titulo": "tirar o lixo"}]
 
 
+@app.get("/")
+def index():
+    return "Lista de tarefas.\nPara ver a lista entre em \\tarefas"
+
 @app.get('/tarefas')
 def listar():
     return TAREFAS
